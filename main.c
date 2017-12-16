3e5e4c3d8c31b9dc84390b2aa311c0805d17cb0f
s1040334 <34589960+s1040334@users.noreply.github.com>
2017-12-16 11:59:06 +0800

    change

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hi");
+	printf("Hi\n");
+	printf("I am new branch");
 	return 0;
 }
